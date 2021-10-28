a07167110a98191126a06a4f93f1545a1ee82d8d
MrNewaz <saifur.saif@northsouth.edu>
2021-10-28 20:39:09 +0600

    here we go

--- 09_dates.php
+++ 09_dates.php
@@ -1,13 +1,3 @@
 <?php
 
-// Print current date
-
-// Print yesterday
-
-// Different format: https://www.php.net/manual/en/function.date.php
-
-// Print current timestamp
-
-// Parse date: https://www.php.net/manual/en/function.date-parse.php
-
-// Parse date from format: https://www.php.net/manual/en/function.date-parse-from-format.php
+echo date("Y-m-d");
--- 10_include_require/02_functions/index.php
+++ 10_include_require/02_functions/index.php
@@ -1,1 +1,5 @@
 <?php
+
+require_once 'math.php';
+
+echo add(5, 8);
